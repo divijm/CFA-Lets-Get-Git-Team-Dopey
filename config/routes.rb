ad084a448de49bb4521ae79ad16aac80b4df3dd9
BinnyK <BinnyK@users.noreply.github.com>
2017-04-07 13:57:17 +1000

    Nest topics under weeks in routes and display topics under each week view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-
   resources :events
 
   resources :questions
 
   resources :topics
   resources :weeks
+  
+  resources :weeks do
+    member do
+      resources :topics
+    end
+  end
   devise_for :users
+  root 'weeks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
