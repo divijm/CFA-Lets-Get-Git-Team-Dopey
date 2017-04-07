e81a40444c6d478123190ff53284f1ee7f229fb1
Bdita <fpbandita@gmail.com>
2017-04-07 14:12:10 +1000

    rolify final, assign default role for new user as student, admin role has to be permitted later

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :assign_default_role
+
+     def assign_default_role
+      self.add_role(:student) if self.roles.blank?
+     end
 end
